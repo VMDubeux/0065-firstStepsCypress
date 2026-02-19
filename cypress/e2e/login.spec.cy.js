638f6068ad48f067a8db7aba15ae679a498b0639
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2026-02-19 17:46:37 -0300

    Second commit

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,10 +1,38 @@
 describe('Orange HRM Tests', () => {
-  it('Login: Passed', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-      cy.get('[name="username"]').type('Admin');
-      cy.get('[name="password"]').type('admin123');
-      cy.get('[type="submit"]').click();
-      cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
-      cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard');
-  })
+    it('Login: Passed', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('[type="submit"]').click();
+        cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
+        cy.get('.oxd-text--h6').contains('Dashboard');
+    })
+
+    it('Login: Failed - Not Registred Username or Password', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admi');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('[type="submit"]').click();
+        cy.get('.oxd-alert-content-text');
+    })
+
+    it('Login: Failed - Username not entered', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="password"]').type('admin123');
+        cy.get('[type="submit"]').click();
+        cy.get('.oxd-text--span');
+    })
+
+    it('Login: Failed - Password not entered', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[name="username"]').type('Admin');
+        cy.get('[type="submit"]').click();
+        cy.get('.oxd-text--span');
+    })
+
+    it('Login: Failed - No credentials', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+        cy.get('[type="submit"]').click();
+        cy.get('.oxd-text--span');
+    })
 })
